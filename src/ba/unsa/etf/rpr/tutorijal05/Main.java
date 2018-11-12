517e1a4530b6848e0fd0d91b0efd0f283e92a5a3
yaly1 <yaly1@etf.unsa.ba>
2018-11-12 22:42:00 +0100

    First commit

--- src/ba/unsa/etf/rpr/tutorijal05/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Main.java
@@ -4,11 +4,36 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
+    /*
+    public void start(Stage primaryStage) throws Exception {
+        StackPane pane = new StackPane();
+        pane.getChildren().add(new Button("OK"));
+        Scene scene = new Scene(pane, 200, 50);
+        primaryStage.setTitle("Button in a pane"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+    }*/
+
+
+    /*public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(new Button("OK"), 200, 250);
+        primaryStage.setTitle("MyJavaFX"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+        Stage stage = new Stage(); // Create a new stage
+        stage.setTitle("Second Stage"); // Set the stage title
+        // Set a scene with a button in the stage
+        stage.setScene(new Scene(new Button("New Stage"), 100, 100));
+        stage.show(); // Display the stage
+    }*/
+
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("digitron.fxml"));
         primaryStage.setTitle("Digitron");
