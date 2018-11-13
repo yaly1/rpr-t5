c6f4cacc10b6a34efbb4367d13af21fe6f4a1780
yaly1 <yaly1@etf.unsa.ba>
2018-11-13 18:23:25 +0100

    digitron functioning but test not ok

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -2,76 +2,188 @@ package ba.unsa.etf.rpr.tutorijal05;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 
 public class Controller {
 
-    public TextField rezultatField;
-    private SimpleStringProperty display;
+    public Label display;
+    private SimpleStringProperty value;
+    private boolean imaTacka = false, imaDrugiBroj = false, imaOperacija = false, imaJednako = false;
+    private double prviBroj, drugiBroj;
+    private char operacija;
+
 
     public Controller() {
-        display = new SimpleStringProperty("0");
+        value = new SimpleStringProperty("0");
     }
 
-    public SimpleStringProperty displayProperty() {
-        return display;
+    public SimpleStringProperty valueProperty() {
+        return value;
     }
 
-    public String getDisplay() {
-        return display.get();
+    public String getValue() {
+        return value.get();
     }
 
-    //@FXML
-    public void jednakoClick(ActionEvent actionEvent) {
-        display.set("0");
-    }
-
-    public void btn1(ActionEvent actionEvent) {
-        if (display.get().equals("0")) display.set("");
-        display.set(display.get() + "1");
+    public void btn1Click(ActionEvent actionEvent) {
+        if (value.get().equals("0") || (imaOperacija && !imaDrugiBroj) || imaJednako) {
+            value.set("");
+            imaJednako = false;
+        }
+        if(imaOperacija)imaDrugiBroj = true;
+        value.set(value.get() + "1");
     }
 
     public void btn2(ActionEvent actionEvent) {
-        if (display.get().equals("0")) display.set("");
-        display.set(display.get() + "2");
+        if (value.get().equals("0") || (imaOperacija && !imaDrugiBroj) || imaJednako) {
+            value.set("");
+            imaJednako = false;
+        }
+        if(imaOperacija )imaDrugiBroj = true;
+        value.set(value.get() + "2");
     }
 
     public void btn3(ActionEvent actionEvent) {
-        if (display.get().equals("0")) display.set("");
-        display.set(display.get() + "3");
+        if (value.get().equals("0") || (imaOperacija && !imaDrugiBroj) || imaJednako) {
+            value.set("");
+            imaJednako = false;
+        }
+        if(imaOperacija )imaDrugiBroj = true;
+        value.set(value.get() + "3");
     }
 
     public void btn4(ActionEvent actionEvent) {
-        if (display.get().equals("0")) display.set("");
-        display.set(display.get() + "4");
+        if (value.get().equals("0") || (imaOperacija && !imaDrugiBroj) || imaJednako) {
+            value.set("");
+            imaJednako = false;
+        }
+        if(imaOperacija )imaDrugiBroj = true;
+        value.set(value.get() + "4");
     }
 
     public void btn5(ActionEvent actionEvent) {
-        if (display.get().equals("0")) display.set("");
-        display.set(display.get() + "5");
+        if (value.get().equals("0") || (imaOperacija && !imaDrugiBroj) || imaJednako) {
+            value.set("");
+            imaJednako = false;
+        }
+        if(imaOperacija )imaDrugiBroj = true;
+        value.set(value.get() + "5");
     }
 
     public void btn6(ActionEvent actionEvent) {
-        if (display.get().equals("0")) display.set("");
-        display.set(display.get() + "6");
+        if (value.get().equals("0") || (imaOperacija && !imaDrugiBroj) || imaJednako) {
+            value.set("");
+            imaJednako = false;
+        }
+        value.set(value.get() + "6");
     }
 
     public void btn7(ActionEvent actionEvent) {
-        if (display.get().equals("0")) display.set("");
-        display.set(display.get() + "7");
+        if (value.get().equals("0") || (imaOperacija && !imaDrugiBroj) || imaJednako) {
+            value.set("");
+            imaJednako = false;
+        }
+        if(imaOperacija )imaDrugiBroj = true;
+        value.set(value.get() + "7");
     }
 
     public void btn8(ActionEvent actionEvent) {
-        if (display.get().equals("0")) display.set("");
-        display.set(display.get() + "8");
+        if (value.get().equals("0") || (imaOperacija && !imaDrugiBroj) || imaJednako) {
+            value.set("");
+            imaJednako = false;
+        }
+        if(imaOperacija )imaDrugiBroj = true;
+        value.set(value.get() + "8");
     }
 
     public void btn9(ActionEvent actionEvent) {
-        if (display.get().equals("0")) display.set("");
-        display.set(display.get() + "9");
+        if (value.get().equals("0") || (imaOperacija && !imaDrugiBroj) || imaJednako) {
+            value.set("");
+            imaJednako = false;
+        }
+        if(imaOperacija )imaDrugiBroj = true;
+        value.set(value.get() + "9");
     }
 
     public void btn0(ActionEvent actionEvent) {
-        if (!display.get().equals("0")) display.set(display.get() + "0");
+        if ((imaOperacija && !imaDrugiBroj) || imaJednako) {
+            value.set("");
+            imaJednako = false;
+        }
+        if(imaOperacija )imaDrugiBroj = true;
+        if (!value.get().equals("0")) value.set(value.get() + "0");
     }
+
+    public void dotBtn(ActionEvent actionEvent) {
+        if ((imaOperacija && !imaDrugiBroj) || imaJednako) {
+            value.set("0.");
+            imaJednako = false;
+            imaTacka = true;
+            return;
+        }
+        if (!imaTacka) value.set(value.get() + ".");
+        imaTacka = true;
+    }
+
+    public void plusBtn(ActionEvent actionEvent) {
+        if(imaDrugiBroj) equalsBtn(actionEvent);
+        prviBroj = Double.parseDouble(value.get());
+        imaDrugiBroj = false;
+        imaOperacija = true;
+        operacija = '+';
+    }
+
+    public void minusBtn(ActionEvent actionEvent) {
+        if(imaDrugiBroj) equalsBtn(actionEvent);
+        prviBroj = Double.parseDouble(value.get());
+        imaDrugiBroj = false;
+        imaOperacija = true;
+        operacija = '-';
+    }
+
+    public void multiplyBtn(ActionEvent actionEvent) {
+        if(imaDrugiBroj) equalsBtn(actionEvent);
+        prviBroj = Double.parseDouble(value.get());
+        imaDrugiBroj = false;
+        imaOperacija = true;
+        operacija = '*';
+    }
+
+    public void divideBtn(ActionEvent actionEvent) {
+        if(imaDrugiBroj) equalsBtn(actionEvent);
+        prviBroj = Double.parseDouble(value.get());
+        imaDrugiBroj = false;
+        imaOperacija = true;
+        operacija = '/';
+    }
+
+    public void percentBtn(ActionEvent actionEvent) {
+        if(imaDrugiBroj) equalsBtn(actionEvent);
+        prviBroj = Double.parseDouble(value.get());
+        imaDrugiBroj = false;
+        imaOperacija = true;
+        operacija = '%';
+    }
+
+    public void equalsBtn(ActionEvent actionEvent) {
+        if(imaOperacija) {
+            imaJednako = true;
+            drugiBroj = Double.parseDouble(value.get());
+            if(operacija == '+')
+                value.set(String.valueOf(prviBroj + drugiBroj));
+            else if(operacija == '-')
+                value.set(String.valueOf(prviBroj - drugiBroj));
+            else if(operacija == '*')
+                value.set(String.valueOf(prviBroj * drugiBroj));
+            else if(operacija == '/')
+                value.set(String.valueOf(prviBroj / drugiBroj));
+            else if(operacija == '%')
+                value.set(String.valueOf(prviBroj % drugiBroj));
+
+            imaOperacija = false;
+            imaTacka = false;
+        }
+    }
+
+
 }
