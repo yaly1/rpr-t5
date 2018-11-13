8cfc0ab6b81ab61c70a6671377fd8173dd316c54
yaly1 <yaly1@etf.unsa.ba>
2018-11-13 04:31:40 +0100

    Methods "btnx" added, x=0,1,...,9

--- src/ba/unsa/etf/rpr/tutorijal05/Controller.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Controller.java
@@ -1,5 +1,77 @@
 package ba.unsa.etf.rpr.tutorijal05;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
 public class Controller {
 
+    public TextField rezultatField;
+    private SimpleStringProperty display;
+
+    public Controller() {
+        display = new SimpleStringProperty("0");
+    }
+
+    public SimpleStringProperty displayProperty() {
+        return display;
+    }
+
+    public String getDisplay() {
+        return display.get();
+    }
+
+    //@FXML
+    public void jednakoClick(ActionEvent actionEvent) {
+        display.set("0");
+    }
+
+    public void btn1(ActionEvent actionEvent) {
+        if (display.get().equals("0")) display.set("");
+        display.set(display.get() + "1");
+    }
+
+    public void btn2(ActionEvent actionEvent) {
+        if (display.get().equals("0")) display.set("");
+        display.set(display.get() + "2");
+    }
+
+    public void btn3(ActionEvent actionEvent) {
+        if (display.get().equals("0")) display.set("");
+        display.set(display.get() + "3");
+    }
+
+    public void btn4(ActionEvent actionEvent) {
+        if (display.get().equals("0")) display.set("");
+        display.set(display.get() + "4");
+    }
+
+    public void btn5(ActionEvent actionEvent) {
+        if (display.get().equals("0")) display.set("");
+        display.set(display.get() + "5");
+    }
+
+    public void btn6(ActionEvent actionEvent) {
+        if (display.get().equals("0")) display.set("");
+        display.set(display.get() + "6");
+    }
+
+    public void btn7(ActionEvent actionEvent) {
+        if (display.get().equals("0")) display.set("");
+        display.set(display.get() + "7");
+    }
+
+    public void btn8(ActionEvent actionEvent) {
+        if (display.get().equals("0")) display.set("");
+        display.set(display.get() + "8");
+    }
+
+    public void btn9(ActionEvent actionEvent) {
+        if (display.get().equals("0")) display.set("");
+        display.set(display.get() + "9");
+    }
+
+    public void btn0(ActionEvent actionEvent) {
+        if (!display.get().equals("0")) display.set(display.get() + "0");
+    }
 }
--- src/ba/unsa/etf/rpr/tutorijal05/Main.java
+++ src/ba/unsa/etf/rpr/tutorijal05/Main.java
@@ -11,34 +11,11 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     @Override
-    /*
-    public void start(Stage primaryStage) throws Exception {
-        StackPane pane = new StackPane();
-        pane.getChildren().add(new Button("OK"));
-        Scene scene = new Scene(pane, 200, 50);
-        primaryStage.setTitle("Button in a pane"); // Set the stage title
-        primaryStage.setScene(scene); // Place the scene in the stage
-        primaryStage.show(); // Display the stage
-    }*/
-
-
-    /*public void start(Stage primaryStage) throws Exception {
-        Scene scene = new Scene(new Button("OK"), 200, 250);
-        primaryStage.setTitle("MyJavaFX"); // Set the stage title
-        primaryStage.setScene(scene); // Place the scene in the stage
-        primaryStage.show(); // Display the stage
-        Stage stage = new Stage(); // Create a new stage
-        stage.setTitle("Second Stage"); // Set the stage title
-        // Set a scene with a button in the stage
-        stage.setScene(new Scene(new Button("New Stage"), 100, 100));
-        stage.show(); // Display the stage
-    }*/
 
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("digitron.fxml"));
         primaryStage.setTitle("Digitron");
         primaryStage.setScene(new Scene(root, 200, 275));
-        //primaryStage.setResizable(false);
         primaryStage.show();
     }
 
